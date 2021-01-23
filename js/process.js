c311d94e01472c4ea3800608bb99f8149cb74f0f
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2021-01-23 04:06:54 -0800

    Update process.js

--- js/process.js
+++ js/process.js
@@ -8,12 +8,17 @@ const data = [
         "projectLink": "https://e-clone-b16ec.web.app/"      
     },
     // {
+    //     "typeOfProject":"Jumia Clone Mini",
+    //     "shortDescription":"Front-end Ecommerce web App",
+    //     "image": "https://i.pinimg.com/564x/dc/51/00/dc5100a29f67ce40f4972172e3a8c236.jpg",
+    //     "projectLink": "https://jumia-clone-4fdfe.web.app/"      
+    // },
+   // {
     //     "typeOfProject":"Noonya Search Engine",
     //     "shortDescription":" Google based Search Engine mimic",
     //     "image": "https://resize.hswstatic.com/w_796/gif/google-update.jpg",
     //     "projectLink": "https://e-clone-b16ec.web.app/"      
     // },
-  
 ];
 
 window.addEventListener('load', (event)=>{
